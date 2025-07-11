53ed92a8a5733e4774aeb59a8ad2667cb7314579
Dipeesha Vartak <164518650+vartakdipeesha@users.noreply.github.com>
2025-07-11 16:05:38 +0530

    Delete logger.py

--- logger.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def log_attempt(email, ip):
-    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    with open("attack_logs.txt", "a") as file:
-        file.write(f"{now} | Email: {email} | IP: {ip}\n")
